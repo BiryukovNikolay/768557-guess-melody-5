76b7e40b7169db3f2274e55f81f66edfd6aeedb0
Nikolay-Biryukov <nbiryukov@kameleoon.com>
2020-09-30 19:00:43 +0300

    components

---
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const WelcomeScreen = (props) => {
+// eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+  const setErrorTextValue = (value) => {
+    if (value === 1) {
+      return `ошибку`;
+    } else if (value > 4 || value === 0) {
+      return `ошибок`;
+    } else {
+      return `ошибки`;
+    }
+  };
+
+  let errorTextValue = setErrorTextValue(errorsCount);
+
+  return (
+    <section className="welcome">
+      <div className="welcome__logo">
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
+      </div>
+      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <h2 className="welcome__rules-title">Правила игры</h2>
+      <p className="welcome__text">Правила просты:</p>
+      <ul className="welcome__rules-list">
+        <li>Нужно ответить на все вопросы.</li>
+        <li>Можно допустить {errorsCount} {errorTextValue}.</li>
+      </ul>
+      <p className="welcome__text">Удачи!</p>
+    </section>
+  );
+};
+
+
+export default WelcomeScreen;
